ad2cf8ae70e48ed395a4e78266bad2fb3fbeb988
Alejandro Piad Morffis <apiad@apiad.net>
2021-07-14 10:27:11 -0400

    Commit inicial

---
+++ CleanTempFiles.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+find . -type f -name *.aux -delete
+
+rm -rf Thesis.bbl Thesis.blg Thesis.lof Thesis.log Thesis.lot Thesis.out Thesis.pdf Thesis.synctex.gz Thesis.toc Thesis.ilg Thesis.loa Thesis.nlo Thesis.nls
